a548e0fcd42e86a19aa485e7e115f15b9bb784e1
Jonathan Kamens <jik@kamens.us>
2020-11-07 13:04:13 -0500

    Add testing on startup

--- background.js
+++ background.js
@@ -8,9 +8,13 @@
 
 var sendDoesSL;
 var working = false;
+// Set to -1 to bypoass startup testing
+var logicTestCount = 4;
+var lastTestTime = undefined;
 
 function log(msg) {
-  console.log(`jik tweaks: ${msg}`);
+  tag = (logicTestCount > 0) ? " [TESTING]" : "";
+  console.log(`jik tweaks${tag}: ${msg}`);
 }
 
 browser.alarms.onAlarm.addListener(checkChol);
@@ -19,13 +23,37 @@ browser.runtime.sendMessage(
   "sendlater3@kamens.us",
   {action: "getPreferences"}).then(startCheckingChol);
 
+async function setAlarm(info) {
+  var lastTestCount = logicTestCount--;
+  if (lastTestCount > 0) {
+    if (info.when) {
+      lastTestTime = info.when;
+    }
+    else {
+      lastTestTime += info.periodInMinutes * 60000;
+    }
+    // The NextChol endpoint is rate-limited
+    await new Promise(r => setTimeout(r, 1000));
+    log(`setAlarm: running checkChol as of ${new Date(lastTestTime)}`);
+    checkChol({}, lastTestTime);
+  }
+  else if (lastTestCount == 0) {
+    await new Promise(r => setTimeout(r, 1000));
+    log('setAlarm: finished testing, returning to real-time');
+    browser.alarms.create({when: Date.now(), periodInMinutes: 1});
+  }
+  else {
+    browser.alarms.create(info);
+  }
+}
+
 function startCheckingChol(prefs) {
   log(`startCheckingChol: prefs.sendDoesSL=${prefs.sendDoesSL}`);
   sendDoesSL = prefs.sendDoesSL;
-  browser.alarms.create({when: Date.now(), periodInMinutes: 1});
+  setAlarm({when: Date.now(), periodInMinutes: 1});
 }
 
-function checkChol(alarm) {
+function checkChol(alarm, asOf) {
   log('Entering checkChol');
   if (working) {
     log('checkChol: working is true, returning');
@@ -37,7 +65,7 @@ function checkChol(alarm) {
   // inverted NextChol query, which will either return 0 if it's
   // currently Shabbat or Yom Tov, or the time in the future when it
   // will next be Shabbat or Yom Tov.
-  NextChol(false, sendDoesSL == false, true, checkCholCallback);
+  NextChol(false, sendDoesSL == false, true, asOf, checkCholCallback);
   log('checkChol: fired NextChol');
 }
 
@@ -46,7 +74,7 @@ function checkCholCallback(retval, error) {
   working = false;
   if (error) {
     log('XHR returned error; trying again in a minute');
-    browser.alarms.create({periodInMinutes: 1});
+    setAlarm({periodInMinutes: 1});
     throw error;
   }
   if (retval == 0) {
@@ -60,13 +88,13 @@ function checkCholCallback(retval, error) {
            sendDoesSL = !sendDoesSL;
            log(`checkCholCallback: flipped sendDoesSL to ${sendDoesSL}, ` +
                'checking again in a minute');
-           browser.alarms.create({periodInMinutes: 1});
+           setAlarm({periodInMinutes: 1});
          });
     return;
   }
   then = new Date(retval * 1000);
   log(`checkColCallback: waiting until ${then} to check again`);
-  browser.alarms.create({when: retval * 1000});
+  setAlarm({when: retval * 1000});
 }
 
 // If callback is truthy, then it's a function which takes two
@@ -74,6 +102,7 @@ function checkCholCallback(retval, error) {
 // string is truthy, then the request failed, and the string explains
 // why. Otherwise, the return value is what this function would have
 // returned if a callback hadn't been specified.
+// asOf is a JavaScript timestamp (i.e., ms since epoch).
 function NextChol(force, inverse, zeroish, asOf, callback) {
     var url = 'https://jik4.kamens.us/cgi-bin/next-chol.cgi?';
     var args = new Array();
@@ -85,6 +114,9 @@ function NextChol(force, inverse, zeroish, asOf, callback) {
     }
     if (zeroish) {
 	args.push("boolean=1");
+    }
+    if (asOf) {
+        args.push(`date=${Math.round(asOf/1000)}`);
     }
     url = url + args.join("&");
 
